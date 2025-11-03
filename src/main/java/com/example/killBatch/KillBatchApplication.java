1889e3d8236fb38993c212815078548d3ec3f104
kkang_h00n <kig2454@gmail.com>
2025-11-03 21:08:09 +0900

    init

---
+++ src/main/java/com/example/killBatch/KillBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.killBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KillBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KillBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/killBatch/KillBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.killBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KillBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
