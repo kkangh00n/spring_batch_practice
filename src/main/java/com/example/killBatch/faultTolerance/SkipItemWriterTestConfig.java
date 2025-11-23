9028eb9fc9875b09dadc681cc07757534271be37
kkang_h00n <kig2454@gmail.com>
2025-11-23 22:16:26 +0900

    19-3. noRollback 처리

--- src/main/java/com/example/killBatch/faultTolerance/SkipItemWriterTestConfig.java
+++ src/main/java/com/example/killBatch/faultTolerance/SkipItemWriterTestConfig.java
@@ -81,6 +81,27 @@ public class SkipItemWriterTestConfig {
                 .faultTolerant()
                 .skip(TestException.class)
                 .skipLimit(2)
+
+                /**
+                 * noRollback
+                 *
+                 * 1. Reader에서 처리
+                 *      - skip이 가능하다면 건너뜀
+                 *      - skip이 불가능하다면 noRollback
+                 *      - (skip과 noRollback이 둘다 설정되어있을 경우, skip 우선)
+                 * 2. Processor에서 처리
+                 *      - noRollback이 아닐 경우, 청크 트랜잭션 롤백 -> 기존의 skip 처리
+                 *      - noRollback일 경우, 청크 트랜잭션 롤백하지 않음 -> 해당 item 건너뜀
+                 *
+                 *      - processor에서는 skip과 noRollback이 모두 설정되어 있어야 함!!
+                 * 3. Writer에서 처리
+                 *      - 스캔 모드의 개별 쓰기 작업 중 발생
+                 *      - skip이 가능할 경우, skip 우선
+                 *      - skip 처리에 포함되지 않은 경우, 없던 일 처리
+                 *
+                 *      - 핵심: skip과 noRollback 둘 다 설정 시, skip만 동작
+                 */
+                .noRollback(TestException.class)
                 .build();
     }
 
